129011854c988d61dee5e0638cdbab6606c0a1f0
marcosxk122 <marcost.santana@hotmail.com>
2023-06-20 08:40:07 -0300

    focus input text border

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import { TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import { Ionicons } from '@expo/vector-icons'
+import Colors from '../../Constants/Colors';
 
 export default function InputText({ handleAddTask }) {
     const [newTask, setNewTask] = useState<string>('');
+    const [presIn, setPresIn] = useState(false);
 
     return (
         <View style={styles.container}>
             <TextInput
                 placeholder='Adicione uma nova tarefa'
-                style={styles.Input}
+                style={[styles.Input, { borderWidth: presIn ? 2 : 0, borderColor: Colors.purple }]}
                 onChangeText={setNewTask}
                 placeholderTextColor='#808080'
                 value={newTask}
+                onFocus={() => setPresIn(true)}
+                onBlur={() => setPresIn(false)}
             />
             <TouchableOpacity style={styles.button} onPress={() => {
                 handleAddTask(newTask)
